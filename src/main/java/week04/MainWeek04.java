18a791a0e807a1aebf68795c73f9d2d0dec4c5f2
giyeon95 <giyeon15@gmail.com>
2020-12-19 22:54:41 +0900

    Disunit property

--- src/main/java/week04/MainWeek04.java
+++ src/main/java/week04/MainWeek04.java
@@ -1,14 +1,14 @@
 package week04;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 import week04.dashboard01.GitIssueBoard;
 import week04.dashboard01.Participant;
 
 public class MainWeek04 {
-
-
-
-    static final String GIT_OAUTH_TOKEN = "c23476df999cbd5e14f0fa17035147ba73f89b38";
 
     public static void main(String[] args) throws Exception {
         runAssignment01();
@@ -20,7 +20,9 @@ public class MainWeek04 {
      * 과제 1. live-study 대시 보드를 만드는 코드를 작성하세요.
      */
     private static void runAssignment01() throws Exception {
-        GitIssueBoard board = GitIssueBoard.create(GIT_OAUTH_TOKEN, "whiteship/live-study");
+
+
+        GitIssueBoard board = GitIssueBoard.create(getGitToken(), "whiteship/live-study");
         List<Participant> participants = board.getParticipants();
 
         participants.forEach(participant -> System.out.println(participant.getProgressString()));
@@ -30,4 +32,13 @@ public class MainWeek04 {
     private static void runAssignment02() throws Exception {
     }
 
+
+
+    private static String getGitToken() throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("application.properties"));
+
+
+        return   properties.getProperty("github.token");
+    }
 }
